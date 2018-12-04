f6a27e80623bdce09d3e7ee0d5eed991e4993563
Nate <nmerrill@udel.edu>
2018-12-04 14:11:53 -0500

    Merge

--- my_app/static/js/searchTags.js
+++ my_app/static/js/searchTags.js
@@ -239,6 +239,36 @@ function testGet() {
 
 function importQuestionsIntoServer()
 {
+    alert("Bleh")
+    var x = document.getElementById("myFile");
+    var txt = "";
+    if ('files' in x) {
+        if (x.files.length == 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = x.files[i];
+                if ('name' in file) {
+                    txt += "name: " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                    txt += "size: " + file.size + " bytes <br>";
+                }
+            }
+        }
+    } 
+    else {
+        if (x.value == "") {
+            txt += "Select one or more files.";
+        } else {
+            txt += "The files property is not supported by your browser!";
+            txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
+        }
+    }
+    document.getElementById("demo").innerHTML = txt;
+
+    /*
     $.ajax({
           url: "/question",
           type: "POST",
@@ -250,4 +280,5 @@ function importQuestionsIntoServer()
                     alert(errMsg);
                 }
         });
+    */
 }
